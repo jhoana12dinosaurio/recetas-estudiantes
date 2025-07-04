1e74cfafd4d14f74f2f5531f0016895143bce99f
Jhoana Aquino <jhoana.aquino10@gmail.com>
2025-07-04 13:49:17 -0500

    actualizacion de pregunta 1

--- src/hooks/useRecipes.ts
+++ src/hooks/useRecipes.ts
@@ -1,10 +1,16 @@
 import { useContext } from 'react';
 import { RecipeContext } from '../context/RecipeContext';
+import { Recipe } from '../types';
 
 export const useRecipes = () => {
   const context = useContext(RecipeContext);
   if (context === undefined) {
     throw new Error('useRecipes debe ser usado dentro de un RecipeProvider');
   }
+  const {Recipes}=context;
+   const [difficultyFilter, setDifficultyFilter] = useState<string | null>(null);
+const filterByDifficulty = (difficulty: 'Fácil' | 'Intermedio' | 'Difícil') => {
+    setDifficultyFilter(difficulty);
+  };
   return context;
 };
